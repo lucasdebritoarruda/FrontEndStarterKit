b15435371a02eea49f13306b8e4445e484041df1
Lucas de Brito <Lucas@Lucas-de-Brito.local>
2018-06-03 22:37:32 -0300

    Cria versão 1 do Starter Kit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+gulp.task('serve', function(){
+
+	browserSync.init({
+		server: './'
+	});
+
+	gulp.watch('./assets/sass/*', ['sass']);
+	gulp.watch('./assets/views/*', ['pug']).on('change', reload);
+});
+
+gulp.task('sass', function(){
+	return gulp.src('./assets/sass/*.scss')
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(gulp.dest('./public/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('pug', function(){
+	return gulp.src('./assets/views/*.pug')
+	.pipe(pug())
+	.pipe(gulp.dest('./'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('default',['serve']);
+
+
