f203fe986bff3400b38996bfb333e85ab8482f62
Lucas de Brito <Lucas@Lucas-de-Brito.local>
2018-07-16 16:26:37 -0300

    Adiciona autoprefixer ao projeto

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 var reload = browserSync.reload;
 
 
@@ -18,6 +19,7 @@ gulp.task('serve', function(){
 gulp.task('sass', function(){
 	return gulp.src('./assets/sass/*.scss')
 	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer())
 	.pipe(gulp.dest('./public/css'))
 	.pipe(browserSync.stream());
 });
